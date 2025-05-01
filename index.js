8c3412eb3a07c61d5639d2cc30867694ba2d8d5b
Dongri  Pavan Kumar <pavankumardongri@gmail.com>
2025-05-01 19:40:31 +0530

    changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 console.log({indexClientUrl:  process.env.FRONTEND_URL})
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL,
+  origin: process.env.FRONTEND_URL || "*",
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
